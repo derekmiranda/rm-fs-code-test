4bfc6d2068a1e48c28951ffc81acbb2033772c21
Derek Miranda <derek.miranda.1992@gmail.com>
2017-08-13 08:40:02 -0700

    Removed unnecessary foo route

--- sampleapp_be/__init__.py
+++ sampleapp_be/__init__.py
@@ -5,7 +5,6 @@ def main(global_config, **settings):
     config = Configurator(settings=settings)
     config.include('pyramid_chameleon')
     config.add_route('home', '/')
-    config.add_route('foo', '/foo')
     config.add_route('data', '/data')
     config.add_static_view(name='static', path='sampleapp_be:assets')
     config.scan('.views')
