5e9f289b2c626763ef076d99387eb31345709897
Derek Miranda <derek.miranda.1992@gmail.com>
2017-08-14 00:30:39 -0700

    Added module to contain field order

---
+++ sampleapp_fe/client/orderedFieldInfo.js
@@ -0,0 +1,32 @@
+const orderedFieldInfo = [
+  {
+    rawName: 'PROP_NAME',
+    label: 'Name',
+  },
+  {
+    rawName: 'ADDRESS',
+    label: 'Address',
+  },
+  {
+    rawName: 'CITY',
+    label: 'City',
+  },
+  {
+    rawName: 'STATE_ID',
+    label: 'State',
+  },
+  {
+    rawName: 'ZIP',
+    label: 'Zip Code',
+  },
+  {
+    rawName: 'MISSING_FIELD_COUNT',
+    label: 'Missing Field Count',
+  },
+  {
+    rawName: 'MISSING_DATA_ENCODING',
+    label: 'Missing Data Encoding',
+  },
+]
+
+export default orderedFieldInfo;
