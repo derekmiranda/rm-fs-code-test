2ffbd48b37d4466aa92715f1fdc725929164274b
Derek Miranda <derek.miranda.1992@gmail.com>
2017-08-14 00:30:20 -0700

    Changed App title

--- sampleapp_fe/client/App.js
+++ sampleapp_fe/client/App.js
@@ -22,7 +22,7 @@ class App extends Component {
   render() {
     return (
       <main>
-        <h1>Hello World!</h1>
+        <h1>Properties</h1>
         <Button fetchData={this._fetchData} text='Fetch Properties' />
         <Table properties={this.state.properties} />
       </main>
