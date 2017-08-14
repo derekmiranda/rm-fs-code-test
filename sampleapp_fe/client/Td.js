2ef2d76e90c4c3c83321e3e34b8be3321ce168c1
Derek Miranda <derek.miranda.1992@gmail.com>
2017-08-14 07:14:49 -0700

    Display 'Missing Data' if no data in field

--- sampleapp_fe/client/Td.js
+++ sampleapp_fe/client/Td.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const missingStyle = {
+  color: 'gray',
+}
+
+const missing = (
+  <span style={missingStyle}>Missing Data</span>
+)
+
 const Td = ({ value }) => (
-  <td>{ value }</td>
+  <td>{ value !== '' ? value : missing }</td>
 )
 
 export default Td;
