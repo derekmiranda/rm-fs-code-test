1cf8e3ca2f68e94f98d991f1ef2f29d502f17b0f
Derek Miranda <derek.miranda.1992@gmail.com>
2017-08-16 23:25:02 -0700

    Moved field processing methods to models

--- sampleapp_be/models.py
+++ sampleapp_be/models.py
@@ -9,6 +9,40 @@ def parse_csv_asset(path):
         properties = list(reader)
         return properties
 
+
+def get_missing_field_count(prop):
+    missing_field_count = reduce(
+        lambda count, field: count if prop[field] else count + 1,
+        prop.keys(),
+        0
+    )
+    return missing_field_count
+
+def get_missing_data_encoding(prop, separator):
+    encoded_num_builder = []
+    curr_num = 0
+    on_columns_with_data = None
+    add_num = lambda num: encoded_num_builder.append(str(num))    
+
+    for field in prop.keys():
+        val = prop[field]
+        field_has_data = bool(val)
+
+        if on_columns_with_data is None:
+            curr_num = 1
+        else:
+            # if in same data state, add 1 to curr_num
+            if on_columns_with_data == field_has_data:
+                curr_num += 1
+            else:
+                # append curr_num to num_builder
+                add_num(curr_num)
+                # reset curr_num to 1
+                curr_num = 1
+        on_columns_with_data = field_has_data
+    add_num(curr_num)
+    return separator.join(encoded_num_builder)
+
 def process_property(property):
     desired_fields = ('PROP_NAME', 'ADDRESS', 'CITY', 'STATE_ID', 'ZIP')
     processed_prop = {field: property[field] for field in desired_fields} 
